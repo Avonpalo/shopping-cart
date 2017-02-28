f59d491531a5032eafe770e926cf533dbd8927a3
avonpalo <avonpalo@aol.com>
2017-02-28 10:16:50 -1000

    css/styles.css, started over

--- js/app.js
+++ js/app.js
@@ -1,1 +1,33 @@
-console.log("hello");
+
+
+var items = [
+				{product: "Finger Toothbrush", 
+				description: "A helping hand to a nicer smile.", 
+				image: "https://www.babyhaven.com/media/extendware/ewimageopt/media/inline/4/9/nuk-gerber-infant-tooth-gum-cleanser-finger-toothbrush-set-cc6.jpeg", 
+				price: 1.11}, 
+
+				{product: "Barry Manilow's Greatest Hits Collection Vol 1", 
+				description: "Music the way it should be! Reminisce the past glory as your ship to happiness has sailed.", 
+				image: "http://direct.rhapsody.com/imageserver/images/Alb.201470/500x500.jpg", 
+				price: 39.57}, 
+
+				{product: "Ramen Oreos", 
+				description: "The overly used cliche 'East Meets West' comes to life as Nabisco has infused the savory flavors of the Far East sandwiched between two chocolate wafers. Dip that in your sake.", 
+				image: "https://cdn1.thehunt.com/app/public/system/zine_images/5085639/original/d04a645495fe24be9451696a2a15b272.jpg", 
+				price: 8.88}, 
+
+				{product: "Woof Washer 360", 
+				description: "Wash your dirty stinky mutt in minutes! Water and dog not included." ,  
+				image: "http://www.asseenontv.com/imgcache/product/resized/000/997/376/catl/woof-washer-360_1000.jpg?k=ad4eadac&pid=997376&s=catl&sn=asseenontv", 
+				price: 9.29}, 
+
+				{product: "Sauna Pants", 
+				description: "Is it hot in here? Indeed. It's my pants. Look cool while losing weight.", 
+				image: "http://images.g4tv.com/rimg_606x0/ImageDb3/292414_l/kevin-pereira-and-candace-bailey-show-off-this-seasons-hottest-fashion-item-sauna-pants.JPG", 
+				price: 2.33}, 
+
+				{product: "Hug Me Pillow", 
+				description: "No more lonely nights as you snuggle with your best friend. And it will never walk out on you.", 
+				image: "http://chirkup.me/images/uploaded/00/00/09/958_490x.jpg", 
+				price: 599.99}
+			];
